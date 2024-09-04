01540fa5b51d7a2f6bbf2869e7fb00ba410e56cd
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-09-04 20:37:11 +0700

    Update code crud nextjs

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com',
+      },
+    ],
+  },
+
+};
+
+export default nextConfig;
