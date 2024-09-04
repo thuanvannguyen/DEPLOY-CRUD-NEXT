1a57ef8c75580161bc7494db7cb58ebfa5a98075
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-09-04 21:12:58 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 // Lay du lieu tu API
 const getData = async () => {
-  const res = await fetch('http://localhost:3000/api/blog', { next: { revalidate: 3600 } });
+  const res = await fetch('deploy-crud-next.vercel.app/api/blog', { next: { revalidate: 3600 } });
 
   if(!res.ok) {
     throw new Error("Databse Error!")
