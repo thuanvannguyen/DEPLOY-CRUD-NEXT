9843282f7cf76b79a968012962a466e9c188e6a0
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-09-04 21:17:59 +0700

    Update page.js

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,10 +1,10 @@
-import { getPost } from '@/lib/data';
+// import { getPost } from '@/lib/data';
 import React from 'react'
 
 const BlogDetail = async({params}) => {
-  const {slug} = params;
-  const postDetail = await getPost(slug);
-  console.log(postDetail);
+  // const {slug} = params;
+  // const postDetail = await getPost(slug);
+  // console.log(postDetail);
   return (
     <div>BlogDetail</div>
   )
