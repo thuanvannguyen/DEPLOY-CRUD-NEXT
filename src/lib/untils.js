08893d1139fb7247f58e8fb606df2ae2dd112513
thuan.nv <nguyenthuankma@gmail.com>
2024-09-05 14:02:23 +0700

    update untils

--- src/lib/untils.js
+++ src/lib/untils.js
@@ -4,11 +4,11 @@ const connection = {};
 
 export const connectToDb = async () => {
   try {
-    if (connection.isConnected) {
+    if(connection.isConnected) {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO);
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
