79855d939d698f64897065bd2e0dc58d8598b948
thuan.nv <nguyenthuankma@gmail.com>
2024-09-05 13:46:13 +0700

    Update untils.js

--- src/lib/untils.js
+++ src/lib/untils.js
@@ -8,8 +8,14 @@ export const connectToDb = async () => {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      //must add in order to not get any error masseges:
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
     connection.isConnected = db.connections[0].readyState;
+    console.log(`mongo database is connected!!! ${conn.connection.host} `)
   } catch (error) {
     console.log(error);
     throw new Error(error);
