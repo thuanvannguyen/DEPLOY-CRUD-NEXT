e5e8f277af1175cfb8ffc44da17737d593576fd5
thuan.nv <nguyenthuankma@gmail.com>
2024-09-05 15:11:30 +0700

    Update route.js

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,16 +1,24 @@
-// API LISTING BLOGS
-
 import { Post } from "@/lib/models";
 import { connectToDb } from "@/lib/untils";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   try {
-    connectToDb();
+    // Ensure database connection
+    await connectToDb();
+
+    // Fetch posts from the database
     const posts = await Post.find();
+
+    // Return the posts as JSON
     return NextResponse.json(posts);
   } catch (err) {
-    console.log(err);
-    throw new Error("Failed to fetch posts!");
+    console.error("Failed to fetch posts:", err);
+
+    // Return an error response with status code 500
+    return NextResponse.json(
+      { message: "Failed to fetch posts!" },
+      { status: 500 }
+    );
   }
 };
